8c4c36ad96aa32a83852b271f6cc0c08572182e2
ste7en <steven@qiyigou.com>
2017-07-13 16:15:19 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,46 @@
-console.log('\'Allo \'Allo!');
+var sidebar = {
+  init: function() {
+    var sidebar = $('.sidebar')
+    $('.sidebar-toggle').click(function() {
+      $(this).toggleClass('open')
+      sidebar.toggleClass('open')
+    })
+  }
+}, home = {
+  init: function() {
+    $('.owl-carousel').owlCarousel({
+      items: 1,
+      dots: false,
+      loop: true,
+      lazyLoad: true,
+      autoplay: true,
+      animateIn: 'fadeIn',
+      animateOut: 'fadeOut'
+    })
+  }
+}, owlSlide = {
+  init: function() {
+    if ($.fn.owlCarousel && $('.owl-carousel').length) {
+      $('.owl-carousel').owlCarousel({
+        items: 3,
+        lazyLoad: true,
+        loop: true,
+        margin: 15,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {items: 1},
+          768: {items: 2},
+          992: {items: 3}
+        }
+      })
+    }
+  }
+};
+
+$(function() {
+  sidebar.init(),
+  $('body').hasClass('home') && home.init(),
+  $('body').hasClass('creative-sub') && owlSlide.init()
+})
